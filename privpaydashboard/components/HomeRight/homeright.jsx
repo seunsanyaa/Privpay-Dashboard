41ac6216bdd4ff5173f2c010a1c26c219871474a
seunsanyaa <seunsanyaa@gmail.com>
2021-12-06 16:21:37 +0200

    chnages

--- privpaydashboard/components/HomeRight/homeright.jsx
+++ privpaydashboard/components/HomeRight/homeright.jsx
@@ -18,7 +18,27 @@ export default function HomeRight() {
 
 <div className={homeRight.rightGrid}>
     <div className={homeRight.grey}>
-        
+<div className={homeRight.userProfile}>
+
+<div className={homeRight.userProfileIcon}>
+    <img className={homeRight.userProfileIconImage} src='https://avatars.githubusercontent.com/u/2838987?v=4' width={30}  />
+
+</div>
+
+    <div className={homeRight.userProfileName}>
+        <h4 className={homeRight.fullName}>
+            Ade Tiger
+        </h4>
+        <p className={homeRight.email}>
+            Adetiger@gmail.com
+        </p>
+
+
+    </div>
+
+
+</div>
+
     </div>
 
     <div className={homeRight.lightGrey}>
--- privpaydashboard/pages/index.js
+++ privpaydashboard/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import {logo,dashboardicon} from './icon'
+
 import NavBar from "../components/NavBar/navbar";
 import HomeRight from "../components/HomeRight/homeright";
 export default function Home() {
@@ -11,6 +11,13 @@ export default function Home() {
 <div className={styles.grid} >
     <NavBar/>
     <HomeRight/>
+
+    <style jsx global>{`
+      body {
+        margin: 0px;
+        padding: 0px;
+      }
+    `}</style>
 </div>
   </>
   )
