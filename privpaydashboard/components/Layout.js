6ea4a449bf7301c6672f4be28bb535d5c26ceeeb
seunsanyaa <seunsanyaa@gmail.com>
2021-12-28 02:05:30 +0200

    responsive

--- privpaydashboard/components/Layout.js
+++ privpaydashboard/components/Layout.js
@@ -1,15 +1,15 @@
 
 import NavBar from "./NavBar/navbar";
 import TopBar from "./TopBar/topbar";
-
+import layout from "./layout.module.scss"
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className={layout.style}>
             <NavBar />
 
             <main>{children}</main>
 
-        </>
+        </div>
     )
 }
